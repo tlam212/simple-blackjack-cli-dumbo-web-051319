444a092c6ee509ab47249d997dfa772384293d50
Toni Lamont <tlam212@gmail.com>
2019-04-17 02:15:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,15 +58,15 @@ end
 
 def runner
   welcome
-  card_total = initial_round
+  total = initial_round
   
-  until card_total > 21 do
-   card_total= hit?(card_total)
-    display_card_total(card_total)
+  until total > 21 do
+   total= hit?(total)
+    display_card_total(total)
   end
   
   
-end_game(card_total)
+end_game(total)
     
 end
     
